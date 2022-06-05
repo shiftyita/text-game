ba9e498cb11bdc2c00cacf2df4b471ea8489a8ec
shiftyita <54118429+shiftyita@users.noreply.github.com>
2022-06-05 18:17:44 +0200

    add feature for mapengine

--- src/main/java/it/shifty/game/engine/AdventureGame.java
+++ src/main/java/it/shifty/game/engine/AdventureGame.java
@@ -1,9 +1,28 @@
 package it.shifty.game.engine;
 
 import it.shifty.game.engine.exception.LoseGameException;
+import it.shifty.game.engine.exception.RoomMisplacedException;
+import it.shifty.game.engine.map.MapEngine;
+import it.shifty.game.engine.map.Room;
 import it.shifty.game.gameobjects.Character;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AdventureGame {
+
+    public static void main(String[] args) throws RoomMisplacedException {
+
+        List<Room> roomList = new ArrayList<>();
+        roomList.add(new Room("NiceRoom", "Figa", 0,1 ));
+        roomList.add(new Room("NiceRoom", "Figa", 2,2 ));
+        roomList.add(new Room("NiceRoom", "Figa", 3,0 ));
+        roomList.add(new Room("NiceRoom", "Figa", 0,3 ));
+
+        MapEngine mapEngine = new MapEngine(roomList, 3,3);
+
+        mapEngine.drawMap();
+    }
 
     public void manageDamage(Character attackingCharacter, Character defendingCharacter) throws LoseGameException {
         int firstDamage = attackingCharacter.getPrimaryWeapon() != null ? attackingCharacter.getPrimaryWeapon().getDamage() : 0;
---
+++ src/main/java/it/shifty/game/engine/map/MapEngine.java
@@ -0,0 +1,97 @@
+package it.shifty.game.engine.map;
+
+import it.shifty.game.engine.exception.RoomMisplacedException;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MapEngine {
+
+    private Room[][] map;
+    private int xSize;
+    private int ySize;
+
+    public MapEngine(List<Room> rooms, int xGrid, int yGrid) throws RoomMisplacedException {
+        xSize = xGrid;
+        ySize = yGrid;
+        placeRooms(rooms);
+        calculateRoomSurroundigs();
+    }
+
+    public void placeRooms(List<Room> rooms) throws RoomMisplacedException {
+        map = new Room[xSize][ySize];
+        for (Room room: rooms) {
+            insertRoomInMap(room, room.getX(), room.getY());
+        }
+    }
+
+    public void check(int x, int y) throws RoomMisplacedException {
+        if ( x < 0 || y < 0 )
+            throw new IndexOutOfBoundsException("Illegal coordinates");
+        if ( x > xSize || y > ySize)
+            throw new RoomMisplacedException("The room cannot fit in the grid");
+    }
+
+    public boolean existsRoomInSpot(int x, int y) {
+        return map[x][y] != null;
+    }
+
+    public Room insertRoomInMap(Room room, int x, int y) throws RoomMisplacedException {
+        check(x,y);
+        //check if it's not already occupied
+        if (existsRoomInSpot(x,y))
+            throw new RoomMisplacedException("A room exists already in the spot ("+x+","+y+")");
+        map[x][y] = room;
+        room.setX(x);
+        room.setY(y);
+        return room;
+
+    }
+
+    public Optional<Room> getRoom(int x, int y) {
+        try {
+            check(x,y);
+            if (existsRoomInSpot(x,y))
+                return Optional.of(map[x][y]);
+            else
+                return Optional.empty();
+        }
+        catch (Exception | RoomMisplacedException e) {
+            return Optional.empty();
+        }
+    }
+
+    public void drawMap() {
+        for (int x = 0; x < xSize; x++) {
+            for (int y = 0; y < ySize; y++) {
+                if (!existsRoomInSpot(x,y))
+                    System.out.println("R");
+                else
+                    System.out.println("O");
+            }
+        }
+    }
+
+    public void calculateRoomSurroundigs() {
+        for (int x = 0; x < xSize; x++) {
+            for (int y = 0; y < ySize; y++) {
+                if (!existsRoomInSpot(x,y))
+                    continue;
+                Room existingRoom = getRoom(x,y).get();
+                //get north
+                Optional<Room> northRoom = this.getRoom(x+1, y);
+                //get south
+                Optional<Room> southRoom = this.getRoom(x-1,y);
+                //get east
+                Optional<Room> eastRoom = this.getRoom(x, y - 1);
+                //get west
+                Optional<Room> westRoom = this.getRoom(x, y + 1);
+
+                northRoom.ifPresent(existingRoom::setN);
+                southRoom.ifPresent(existingRoom::setS);
+                eastRoom.ifPresent(existingRoom::setE);
+                westRoom.ifPresent(existingRoom::setW);
+            }
+        }
+    }
+}
